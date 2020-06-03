134c363c85923aa92bec38d8bbffcee0ed0c3007
chillgu <bbonggu92@gmail.com>
2020-06-03 21:31:06 +0900

    clone redux-tutorials

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/modules';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
+console.log(store.getState());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+
+    document.getElementById('root'));
 registerServiceWorker();
