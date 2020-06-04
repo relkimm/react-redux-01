86f74946931b7182e28668f5f2bf7cdcbce577d0
chillgu <bbonggu92@gmail.com>
2020-06-04 20:10:04 +0900

    added waiting modules

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
+import waiting from './waiting';
 
 const rootReducer = combineReducers({
-    counter
+    counter,
+    waiting
 });
 
 export default rootReducer;
